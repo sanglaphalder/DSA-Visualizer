50fbeaaa6ebd6e9ee153281ab1c014858afbec54
PseudoCoder-png <subhasishghosh200521@gmail.com>
2026-02-17 19:55:09 +0530

    Added Insertion sort

--- src/algorithms/Searching/linearSearch.js
+++ src/algorithms/Searching/linearSearch.js
@@ -72,3 +72,27 @@ int main() {
     }
     return 0;
 }`;
+export const linearSearchJAVA = `public class Main {
+
+    static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 50, 30, 70, 80, 20};
+        int target = 30;
+
+        int result = linearSearch(arr, target);
+
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+}`;
